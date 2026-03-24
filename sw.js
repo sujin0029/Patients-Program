db47d1cb89e58cb9cc285a565f723e7e19db6c8b
sujin0029 <suujin1029@gmail.com>
2026-03-24 11:35:18 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,20 +1,14 @@
-const CACHE = 'pm-v1'
-const ASSETS = ['/', '/index.html']
+const CACHE = 'bcs-v2'
+const ASSETS = ['/', '/mobile.html', '/index.html']
 
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)))
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS).catch(() => {})))
   self.skipWaiting()
 })
-
 self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys =>
-    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-  ))
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))))
   self.clients.claim()
 })
-
 self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(cached => cached || fetch(e.request).catch(() => caches.match('/index.html')))
-  )
+  e.respondWith(caches.match(e.request).then(cached => cached || fetch(e.request).catch(() => caches.match('/mobile.html'))))
 })
